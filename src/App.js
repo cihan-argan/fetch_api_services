883237c84eac38a76d68f30451594e53357c9b06
Cihan Argan <cihan.argan@outlook.com>
2022-09-21 14:48:24 +0300

    Fetch Api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState(false);
+  const addPost = (data) => {
+    const headers = new Headers();
+    //  headers.append("Content-type", "application/json");
+    headers.append("Authorization", "Bearer: 123456789");
+
+    const formData = new FormData();
+    formData.append("userId", data.userId);
+    formData.append("title", data.title);
+    formData.append("body", data.body);
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      //  body: JSON.stringify(data),
+      body: formData,
+      headers,
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        if (res.ok && res.status === 200) {
+          return res.json();
+        }
+      })
+      .then((data) => setUsers(data))
+      .catch((err) => console.log(err));
+
+    addPost({
+      userId: 1,
+      title: "Örnek Post",
+      body: "Post İçeriği ",
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>User List</h1>
+      <ul>
+        {users && users.map((user) => <li key={user.id}>{user.name}</li>)}
+      </ul>
+    </>
   );
 }
 
